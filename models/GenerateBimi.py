bd7f6a1921a0dc03f3b3df51ac1df353050095e6
unknown <hiteshpandey00@gmail.com>
2020-09-14 21:22:19 +0530

    minor updates

--- models/GenerateBimi.py
+++ models/GenerateBimi.py
@@ -5,5 +5,5 @@ class GenerateBimi():
         else:
             vmc = ""
 
-        bimi_txt_record = "_bimi."+domain+" TXT v=BIMI1; \nl="+svg+vmc
+        bimi_txt_record = "default._bimi."+domain+" TXT v=BIMI1; \nl="+svg+vmc
         return {"status":True, "record":bimi_txt_record,"errors":[""] , "message":"BIMI record generated successfully!!!"}
